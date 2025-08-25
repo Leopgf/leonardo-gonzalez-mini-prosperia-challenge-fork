a4004127927144cb17d6e4f644eb267c97828817
leopgf <leonprimo.9@gmail.com>
2025-08-25 09:03:32 -0400

    return point before change docker config

--- src/services/ocr/ocr.tesseract.ts
+++ src/services/ocr/ocr.tesseract.ts
@@ -1,21 +1,29 @@
+import { convertPdfToImage } from './ocr.aux.js';
 import { OcrProvider } from './ocr.interface.js';
 import { createWorker } from 'tesseract.js';
 
 export class TesseractOcr implements OcrProvider {
-  async extractText({ filePath, mimeType }: { filePath: string; mimeType: string }) {
-
+  async extractText({
+    filePath,
+    mimeType
+  }: {
+    filePath: string;
+    mimeType: string;
+  }) {
     const worker = await createWorker('eng+spa');
 
-    let file = filePath
-    if(mimeType === 'application/pdf') {
-      file = convertPdfToImage(filePath)
-    }
-    const { data } = await worker.recognize(filePath);
+    let imageFilePath = filePath;
 
-    // worker.terminate();
+    if (mimeType === 'application/pdf') {
+      console.log('IS A PDF');
+      imageFilePath = await convertPdfToImage(filePath);
+    }
+
+    console.log('\n\nThis is the final result of imageFilePath', imageFilePath);
+    const { data } = await worker.recognize(imageFilePath);
+
+    worker.terminate();
 
     return { text: '', confidence: 0 };
   }
 }
-
-
